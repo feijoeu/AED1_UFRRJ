d398f5f466c69ceccb116825d3959321efe0e45d
João Vítor da F. Feijó <joaofeijo@pop-os.localdomain>
2022-07-15 23:09:32 -0300

    lista 1

---
+++ Lista de Exercícios 1/filas.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct {
+    int cpf ;
+    char nome[50];
+    char endereco[50];
+    char tel[10];
+    float saldo;
+} tcliente;
+
+typedef struct {
+    int capacidade;
+    tcliente *dados;
+    int primeiro;
+    int ultimo;
+    int nItens;
+} fila;
---
+++ Lista de Exercícios 1/lista linear.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct {
+    int cpf ;
+    char nome[50];
+    char endereco[50];
+    char tel[10];
+    float saldo;
+} tcliente;
+
+typedef struct {
+    int capacidade;
+    tcliente *dados;
+    int ultimo;
+} lista;
